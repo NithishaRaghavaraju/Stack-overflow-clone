4a17b8c39333bd8d6c6610809316989e72c30c80
NithuNIthisha <nithisha2201@gmail.com>
2023-02-07 17:19:23 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# "npm install"
+use the above command on the client and server side to install dependencies
+
+
+# "npm start "
+use the above command on the server and client side 
---
+++ server/index.js
@@ -0,0 +1,33 @@
+import express, { request } from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+import dotenv from "dotenv"
+import userRoutes from "./routes/user.js"
+import questionRoutes from "./routes/Questions.js"
+import answerRoutes from "./routes/Answers.js"
+
+dotenv.config();
+const app = express();
+app.use(express.json({limit : "30mb", extended : true}))
+app.use(express.urlencoded({limit : "30mb", extended : true}))
+app.use(cors())
+
+app.get('/',(req,res)=> {
+     res.send("This is a stack overflow clone API")
+
+})
+
+app.use("/user",userRoutes)
+app.use("/questions",questionRoutes)
+app.use("/answer",answerRoutes)
+
+
+const PORT = process.env.PORT || 5000
+
+const CONNECION_URL = process.env.CONNECTION_URL
+
+
+mongoose.connect( CONNECION_URL, {useNewUrlParser: true, useUnifiedTopology: true} )
+    .then(() => app.listen(PORT, () => console.log(`server running on port ${PORT}`)))
+    .catch((err)=> console.log(err.message))
+
